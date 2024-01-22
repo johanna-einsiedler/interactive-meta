13c9a28f8ebd63290f148ffd0fa3fd5e09689de9
johanna-einsiedler <42142250+johanna-einsiedler@users.noreply.github.com>
2024-01-22 11:18:07 +0100

    update count

--- metadat_examples/get_number_of_studies/get_number.R
+++ metadat_examples/get_number_of_studies/get_number.R
@@ -5,8 +5,6 @@ library(jsonlite)
 library(dplyr)
 #print(getwd())
 
-source('utils.R')
-source('meta_analysis.R')
 
 myArgs <- commandArgs(trailingOnly = TRUE)
 
@@ -15,6 +13,18 @@ study_name<- myArgs[1]
 # get input json
 input <- myArgs[2]
 
+get_this_file <- function() {
+  commandArgs() %>%
+    tibble::enframe(name = NULL) %>%
+    tidyr::separate(
+      col = value, into = c("key", "value"), sep = "=", fill = "right"
+    ) %>%
+    dplyr::filter(key == "--file") %>%
+    dplyr::pull(value)
+}
+this_file <- get_this_file()
+
+source(paste0(strsplit(this_file,'/')[[1]][1],'/calculation_scripts/utils.R'))
 
 
 calculate_number<- function(dataset,input){
@@ -26,5 +36,5 @@ calculate_number<- function(dataset,input){
   return(dim(dataset)[1])
 }
 
-
-
+out = calculate_number(dataset,input)
+print(out)
