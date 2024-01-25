ab4234ce2d9df8065494b027147ae6ca7c9db7b0
johanna-einsiedler <42142250+johanna-einsiedler@users.noreply.github.com>
2024-01-25 01:33:37 +0100

    Iamstupid

--- metadat_examples/api_calculation/run_analysis.R
+++ metadat_examples/api_calculation/run_analysis.R
@@ -35,28 +35,24 @@ filename <- myArgs[2]
 #inputs <- fromJSON(paste0('/Users/htr365/Documents/Side_Projects/09_founding_lab/semester_project/meta-studies/metadat_examples/filter_descriptions/',study_name,'.txt'))
 #input <- toJSON(input)
 
-get_this_file <- function() {
-  commandArgs() %>%
-    tibble::enframe(name = NULL) %>%
-    tidyr::separate(
-      col = value, into = c("key", "value"), sep = "=", fill = "right"
-    ) %>%
-    dplyr::filter(key == "--file") %>%
-    dplyr::pull(value)
-}
-this_file <- get_this_file()
+
 #input <- fromJSON(paste0(strsplit(this_file,'/')[[1]][1],'/filter_descriptopns/',study_name,'.txt'))
 #print(paste0(strsplit(this_file,'/')[[1]][1],'/custom_filters/',filename))
-input <- fromJSON(paste0(strsplit(this_file,'/')[[1]][1],'/custom_filters/',filename))
-# <- fromJSON('/Users/htr365/Documents/Side_Projects/09_founding_lab/semester_project/meta-studies/metadat_examples/api_calculation/custom_filters/faaae074-5f6a-4966-ba68-d12db18b96a8.json')
+#print(paste0(strsplit(this_file,'/')[[1]][1],'/custom_filters/'))
+input <- fromJSON(paste0(strsplit(this_file,'/')[[1]][1],'/',strsplit(this_file,'/')[[1]][2],'/custom_filters/',filename))
+#input<- fromJSON('/Users/htr365/Documents/Side_Projects/09_founding_lab/semester_project/meta-studies/metadat_examples/api_calculation/custom_filters/55d95701-936b-422f-8e44-22691751fcaa.json')
 #input <- fromJSON('/Users/htr365/Documents/Side_Projects/09_founding_lab/semester_project/meta-studies/metadat_examples/api_calculation/custom_filters/bangertowns_test.json')
+
 
 calculate_meta <- function(study_name,input){
   # get relevant function
   function_name <- paste0('f.',str_split(study_name,'\\.')[[1]][2])
+  print('test')
+  
   # filter dataset
   dataset <- eval(as.symbol(study_name))
   dataset <- filter_data(dataset,input)
+
   #print(dataset)
   # run analysis 
   res <- eval(as.symbol(function_name))(dataset)
@@ -65,7 +61,7 @@ calculate_meta <- function(study_name,input){
   return(out)
 }
 
- out = calculate_meta(dataset,input)
+out = calculate_meta(study_name,input)
 print(out)
 
 
