a3690bf3cbb8cf13e8fd1c64256c8d91ba0a54bc
johanna-einsiedler <42142250+johanna-einsiedler@users.noreply.github.com>
2024-01-25 01:00:44 +0100

    updated

--- metadat_examples/api_calculation/run_analysis.R
+++ metadat_examples/api_calculation/run_analysis.R
@@ -5,15 +5,29 @@ library(jsonlite)
 library(dplyr)
 #print(getwd())
 
-source('utils.R')
-source('meta_analysis.R')
+get_this_file <- function() {
+  commandArgs() %>%
+    tibble::enframe(name = NULL) %>%
+    tidyr::separate(
+      col = value, into = c("key", "value"), sep = "=", fill = "right"
+    ) %>%
+    dplyr::filter(key == "--file") %>%
+    dplyr::pull(value)
+}
+this_file <- get_this_file()
+
+source(paste0(strsplit(this_file,'/')[[1]][1],'/api_calculation/utils.R'))
+source(paste0(strsplit(this_file,'/')[[1]][1],'/api_calculation/meta_analysis.R'))
+
+#source('utils.R')
+#source('meta_analysis.R')
 
 myArgs <- commandArgs(trailingOnly = TRUE)
 
 # get study name
 study_name<- myArgs[1]
 # get input json
-#input <- myArgs[2]
+filename <- myArgs[2]
 #print(input)
 # test
 #study_name <- 'dat.aloe2013'
@@ -31,10 +45,13 @@ get_this_file <- function() {
     dplyr::pull(value)
 }
 this_file <- get_this_file()
-input <- fromJSON(paste0(strsplit(this_file,'/')[[1]][1],'/filter_descriptiopns/',study_name,'.txt'))
+#input <- fromJSON(paste0(strsplit(this_file,'/')[[1]][1],'/filter_descriptopns/',study_name,'.txt'))
+#print(paste0(strsplit(this_file,'/')[[1]][1],'/custom_filters/',filename))
+input <- fromJSON(paste0(strsplit(this_file,'/')[[1]][1],'/custom_filters/',filename))
+# <- fromJSON('/Users/htr365/Documents/Side_Projects/09_founding_lab/semester_project/meta-studies/metadat_examples/api_calculation/custom_filters/faaae074-5f6a-4966-ba68-d12db18b96a8.json')
+#input <- fromJSON('/Users/htr365/Documents/Side_Projects/09_founding_lab/semester_project/meta-studies/metadat_examples/api_calculation/custom_filters/bangertowns_test.json')
 
-
-calculate_meta <- function(dataset,input){
+calculate_meta <- function(study_name,input){
   # get relevant function
   function_name <- paste0('f.',str_split(study_name,'\\.')[[1]][2])
   # filter dataset
@@ -50,4 +67,5 @@ calculate_meta <- function(dataset,input){
 
  out = calculate_meta(dataset,input)
 print(out)
+
 
